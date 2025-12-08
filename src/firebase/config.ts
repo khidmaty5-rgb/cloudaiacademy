ae97f7016f47b388f087c9add004fb71199fd790
NGnif <nagnifda@gmail.com>
2025-12-08 16:36:55 -0500

    chore: bump next to patched version

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-3170120655-4bab7",
-  "appId": "1:432146246011:web:4eade65bf665bcc715b5de",
-  "apiKey": "AIzaSyDtVZyvaEJ6FDm5bZuUzfEYLLyZeOqBgZg",
-  "authDomain": "studio-3170120655-4bab7.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "432146246011"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDtVZyvaEJ6FDm5bZuUzfEYLLyZeOqBgZg",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-3170120655-4bab7.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-3170120655-4bab7",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:432146246011:web:4eade65bf665bcc715b5de",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "432146246011",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || "",
 };
